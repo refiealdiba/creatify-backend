012b627e22d19ffe501703477c4b335fef4c630c
pokemonnnn <93369245+pokemonnnn@users.noreply.github.com>
2025-07-11 06:11:34 +0700

    fix db conn

--- user-service/src/config/db.ts
+++ user-service/src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const db = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
+    password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
 });
 
